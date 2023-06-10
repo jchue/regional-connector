8dc81310103491bef32032469105a61c5b9324a4
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2023-06-09 17:32:54 -0700

    Add Vercel Web Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const app = createApp(App)
 
